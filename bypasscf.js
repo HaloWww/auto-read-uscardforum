a6c173bc90611f409ea9615aefc780fb88c64ffe
HaloWww <64211024+HaloWww@users.noreply.github.com>
2026-03-15 11:20:11 +0800

    Update bypasscf.js

--- bypasscf.js
+++ bypasscf.js
@@ -307,10 +307,10 @@ async function launchBrowserForUser(username, password) {
     browser = newBrowser; // 将 browser 初始化
     // 设置浏览器窗口/视口大小（宽度x高度，单位：像素）
     // 可选分辨率：1920x1080（Full HD）、1366x768（笔记本）、1440x900（MacBook）
-    await page.setViewport({
-      width: 1080,
-      height: 1920
-    });
+    // await page.setViewport({
+    //   width: 1080,
+    //   height: 1920
+    // });
     // 启动截图功能
     // takeScreenshots(page);
     //登录操作
